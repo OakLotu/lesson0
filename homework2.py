331566ae14f23f920a0aae33165d245ed47f4dd5
OakLotu <danyakalinin1201@gmail.com>
2024-07-16 11:26:50 +0300

    Delete homework2.py

--- homework2.py
+++
@@ -1,5 +0,0 @@
-Homework = 12
-Hours = 1.5
-name = 'Python'
-average_value = Hours / Homework
-print ( 'Курс:', name, ', всего задач: ', Homework, ', затрачено часов: ', Hours, ', среднее время выполнения:' ,average_value )
